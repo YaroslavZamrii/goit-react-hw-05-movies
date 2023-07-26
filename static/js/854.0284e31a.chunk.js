adf56572ef197184c370d86551a60c3d54f1cbe5
YaroslavZamrii <119421294+YaroslavZamrii@users.noreply.github.com>
2023-07-26 10:09:54 +0000

    Deploying to gh-pages from @ YaroslavZamrii/goit-react-hw-05-movies@3cb058ee9454fd97cc58a10e351423f8c1d0893c 🚀

---
+++ static/js/854.0284e31a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[854],{854:function(e,a,t){t.r(a);var r=t(691),s=t(184);a.default=function(){return(0,s.jsx)(r.Yt,{visible:!0,height:"80",width:"80",ariaLabel:"MagnifyingGlass-loading",wrapperStyle:{margin:"0 auto",position:"fixed",top:"50%",left:"50%",transform:"translate(-50%, -50%)"},wrapperClass:"MagnifyingGlass-wrapper",glassColor:"#c0efff",color:"#e15b64"})}}}]);
+//# sourceMappingURL=854.0284e31a.chunk.js.map
